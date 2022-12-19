284d9d66f3bf6719aa0bbcbc7b8ec8ebe06cdce9
Nguyen Duc Toan <ntoan96@gmail.com>
2022-12-20 02:50:07 +0700

    From top level statements to class structure

--- FlashPatch/Program.cs
+++ FlashPatch/Program.cs
@@ -1,42 +1,53 @@
-﻿using FlashPatch;
-using Tomlyn;
+﻿using Tomlyn;
 
-string configText = File.ReadAllText(@"D:\Code\cshaftprojects\FlashPatch\FlashPatch\patch.toml");
-var config = Toml.ToModel<PatchConfig>(configText);
-
-DirectoryInfo outDir = Directory.CreateDirectory(config.OutDir);
-foreach (var child in outDir.GetFiles())
+namespace FlashPatch
 {
-    child.Delete();
-}
-
-List<Task> tasks = new();
-
-foreach (string file in GlobUtils.GetFiles(config.Root, config.GlobalIncludes, config.GlobalExcludes))
-{
-    tasks.Add(Task.Run(() =>
+    class Program
     {
-
-        string content = File.ReadAllText(file);
-        bool fileChanged = false;
-        foreach (Patch patch in config.Patches)
+        public static void Main(string[] args)
         {
-            if (patch.IsMatch(file))
+
+            string configText = File.ReadAllText(@"D:\Code\cshaftprojects\FlashPatch\FlashPatch\patch.toml");
+            var config = Toml.ToModel<PatchConfig>(configText);
+
+            DirectoryInfo outDir = Directory.CreateDirectory(config.OutDir);
+            foreach (var child in outDir.GetFiles())
             {
-                (bool applied, string? newContent) = patch.ApplyPatch(content);
-                if (applied)
+                child.Delete();
+            }
+
+            List<Task> tasks = new();
+
+            foreach (string file in GlobUtils.GetFiles(config.Root, config.GlobalIncludes, config.GlobalExcludes))
+            {
+                tasks.Add(Task.Run(() => ApplyPatches(file, config.Patches, config.OutDir)));
+            }
+
+            Task.WaitAll(tasks.ToArray());
+        }
+
+        private static void ApplyPatches(string file, IEnumerable<Patch> patches, string outDir)
+        {
+
+            string content = File.ReadAllText(file);
+            bool fileChanged = false;
+            foreach (Patch patch in patches)
+            {
+                if (patch.IsMatch(file))
                 {
-                    content = newContent!;
-                    fileChanged = true;
+                    (bool applied, string? newContent) = patch.ApplyPatch(content);
+                    if (applied)
+                    {
+                        content = newContent!;
+                        fileChanged = true;
+                    }
                 }
             }
+            if (fileChanged)
+            {
+                File.WriteAllText(Path.Join(outDir, Path.GetFileName(file)), content);
+                Console.WriteLine($"Apply patches to file {file}");
+            }
         }
-        if (fileChanged)
-        {
-            File.WriteAllText(Path.Join(config.OutDir, Path.GetFileName(file)), content);
-            Console.WriteLine($"Apply patches to file {file}");
-        }
-    }));
+    }
 }
-
-Task.WaitAll(tasks.ToArray());
